688c833cb8b2e322974702d6c5aa050f09206d56
Piyush <167883369+PiyushWadhwa92@users.noreply.github.com>
2026-05-02 00:38:10 +0530

    Add files via upload

---
+++ frontend/frontend/src/services/taskService.js
@@ -0,0 +1,23 @@
+﻿import api from './api';
+
+export const getTasks = async (filters = {}) => {
+  const params = new URLSearchParams(filters).toString();
+  const response = await api.get(`/tasks${params ? `?${params}` : ''}`);
+  return response.data;
+};
+
+export const getDashboardStats = async () => {
+  const response = await api.get('/tasks/dashboard');
+  return response.data;
+};
+
+export const createTask = async (taskData) => {
+  const response = await api.post('/tasks', taskData);
+  return response.data;
+};
+
+
+export const updateTask = async (id, taskData) => {
+  const response = await api.put(`/tasks/${id}`, taskData);
+  return response.data;
+};
